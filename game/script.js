175c0d5f25b38b30c4f5219ae1325a2d47278e63
valenlyn <valenlynchua@gmail.com>
2019-03-24 18:56:39 +0800

    Added new art, grouped mole-making into functions

--- game/script.js
+++ game/script.js
@@ -1,6 +1,7 @@
 console.log("Linked");
 
-var score = 0;
+var score = 5;
+var round = 1;
 
 var info = document.getElementsByClassName('info')[0];
 var displayScore = document.createElement('p');
@@ -11,20 +12,20 @@ info.appendChild(displayScore);
 var area = document.getElementById('main');
 var boxes = [];
 
-var round = 1;
 var roundInfo = document.getElementsByClassName('round')[0];
 var displayRound = document.createElement('p');
 displayRound.innerText = "Round: " + round;
 roundInfo.appendChild(displayRound);
+
 var roundMessage = document.createElement('span');
-roundMessage.innerText = "Hit 10 rowlets";
+roundMessage.innerText = "Hit 7 rowlets";
 roundInfo.appendChild(roundMessage);
 
 
 
 function createDivs() {
 
-    for (var i = 0; i <= 130; i++) {
+    for (var i = 0; i <= 45; i++) {
         var div = document.createElement('div');
         div.setAttribute('class','box');
         div.innerText = " ";
@@ -32,16 +33,16 @@ function createDivs() {
         boxes.push(div);
 
         var moles = document.createElement('img');
-        moles.setAttribute('src','rowlet.png');
-        moles.setAttribute('style','width:20px;height:20px;display:none');
+        moles.setAttribute('src','sad-mole.png');
+        moles.setAttribute('style','width:50px;height:50px;display:none');
         moles.setAttribute('class','mole');
         // moles.setAttribute('style','height:20px');
         // moles.setAttribute('style','display:none');
         div.appendChild(moles);
 
         var pimples = document.createElement('img');
-        pimples.setAttribute('src','weedle.gif');
-        pimples.setAttribute('style','width:20px;height:20px;display:none');
+        pimples.setAttribute('src','happy-mole.png');
+        pimples.setAttribute('style','width:50px;height:50px;display:none');
         pimples.setAttribute('class','pimples')
         div.appendChild(pimples);
 
@@ -63,6 +64,14 @@ function hit() {
     displayScore.innerText= "Score: " + score;
     // var parent = this.parentNode;
     // parent.removeChild(this);
+
+// Display points earned
+    // var pointDisplay = document.createElement('span');
+    // pointDisplay.setAttribute('class','point');
+    // pointDisplay.innerText = "+1";
+    // this.parentNode.insertBefore(pointDisplay, this);
+    // setTimeout(function(){pointDisplay.setAttribute('style','display:none')},1000);
+
 }
 
 
@@ -77,57 +86,30 @@ function badHit() {
     displayScore.innerText= "Score: " + score;
 }
 
-function makeMoles1000() {
-
-    var randomNumber = Math.floor(Math.random() * 130) + 1;
+function makeMoles(timeToAppear,timeToDisappear) {
+    setTimeout(function(){
+        var randomNumber = Math.floor(Math.random() * 45) + 1;
     document.getElementsByClassName('mole')[randomNumber].setAttribute('style','display:inline');
     document.getElementsByClassName('mole')[randomNumber].addEventListener('click',hit);
 
     setTimeout(function(){
-        document.getElementsByClassName('mole')[randomNumber].setAttribute('style','display:none')}, 1000);
-
+        document.getElementsByClassName('mole')[randomNumber].setAttribute('style','display:none')}, timeToDisappear);
+    },timeToAppear)
 }
 
-function makeMoles3000() {
-
-    var randomNumber = Math.floor(Math.random() * 130) + 1;
-    document.getElementsByClassName('mole')[randomNumber].setAttribute('style','display:inline');
-    document.getElementsByClassName('mole')[randomNumber].addEventListener('click',hit);
+function makeHappyMoles(timeToAppear,timeToDisappear){
+    setTimeout(function(){
+            var randomNumber = Math.floor(Math.random() * 30) + 1;
+    document.getElementsByClassName('pimples')[randomNumber].setAttribute('style','display:inline');
+    document.getElementsByClassName('pimples')[randomNumber].addEventListener('click',badHit);
 
     setTimeout(function(){
-        document.getElementsByClassName('mole')[randomNumber].setAttribute('style','display:none')}, 3000);
-
+        document.getElementsByClassName('pimples')[randomNumber].setAttribute('style','display:none')}, timeToDisappear);
+},timeToAppear)
 }
 
-function makeMoles4000() {
-
-    var randomNumber = Math.floor(Math.random() * 130) + 1;
-    document.getElementsByClassName('mole')[randomNumber].setAttribute('style','display:inline');
-    document.getElementsByClassName('mole')[randomNumber].addEventListener('click',hit);
-
-    setTimeout(function(){
-        document.getElementsByClassName('mole')[randomNumber].setAttribute('style','display:none')}, 4000);
-
-}
-
-
-function roundOne() {
-    makeMoles3000();
-    makeMoles4000();
-    setTimeout(makeMoles4000, 3000);
-    setTimeout(makeMoles4000, 1000);
-    setTimeout(makeMoles4000, 2000);
-    setTimeout(makeMoles4000, 5000);
-    setTimeout(makeMoles3000, 1000);
-    setTimeout(makeMoles3000, 4000);
-    setTimeout(makeMoles1000, 4000);
-    setTimeout(makeMoles1000, 2000);
-}
-
-roundOne();
-
-function makePimples3000() {
-    var randomNumber = Math.floor(Math.random() * 130) + 1;
+function makeHappyMoles3000() {
+    var randomNumber = Math.floor(Math.random() * 30) + 1;
     document.getElementsByClassName('pimples')[randomNumber].setAttribute('style','display:inline');
     document.getElementsByClassName('pimples')[randomNumber].addEventListener('click',badHit);
 
@@ -135,7 +117,181 @@ function makePimples3000() {
         document.getElementsByClassName('pimples')[randomNumber].setAttribute('style','display:none')}, 3000);
 }
 
-makePimples3000();
-setTimeout(makePimples3000, 2000);
-setTimeout(makePimples3000, 2000);
-setTimeout(makePimples3000, 3000);
+
+function roundOne() {
+
+    roundMessage.innerText = "Whack 5 unhappy moles";
+
+    makeMoles(2200,2000);
+    makeMoles(1500,3000);
+    makeMoles(1000,4000);
+    makeMoles(3000,2000);
+    makeMoles(2800,2000);
+    makeMoles(2000,3000);
+    makeMoles(1900,3000);
+
+    var c = 0;
+    var t;
+
+    function timedCount() {
+
+        if (c <= 9) {
+            document.getElementById("timer").innerText = "00:0" + c;
+        } else if (c > 9) {
+             document.getElementById("timer").innerText = "00:" + c;
+        }
+
+
+        c = c + 1;
+        t = setTimeout(timedCount, 1000);
+
+            if (c === 6) {
+            clearTimeout(t);
+
+                if (score >= 5) {
+                    function message() {
+                        console.log("Won");
+                    round++;
+                    setTimeout(roundTwo,1500);
+
+                    }
+
+                    setTimeout(message,1000);
+
+
+                } else {
+                    console.log("Failed")
+                }
+            }
+
+    }
+
+    timedCount();
+
+}
+
+function roundTwo() {
+
+displayRound.innerText = "Round: " + round;
+roundMessage.innerText = "Whack 10 unhappy moles, don't whack the happy ones!";
+
+makeMoles(1000,4000);
+makeMoles(2000,3000);
+makeMoles(2500,3000);
+makeMoles(3000,2500);
+makeMoles(3500,3000);
+makeMoles(4000,1500);
+makeMoles(4500,2000);
+makeMoles(5000,2000);
+makeMoles(6000,1500);
+makeMoles(6500,2000);
+makeMoles(7000,1000);
+makeMoles(7500,1000);
+makeMoles(8000,2000);
+makeMoles(8500,1300);
+makeMoles(9000,1000);
+
+makeHappyMoles(2000,3000);
+makeHappyMoles(5500,3000);
+makeHappyMoles(6500,3000);
+
+    var c = 0;
+    var t;
+
+    function timedCount() {
+
+        if (c <= 9) {
+            document.getElementById("timer").innerText = "00:0" + c;
+        } else if (c > 9) {
+             document.getElementById("timer").innerText = "00:" + c;
+        }
+
+
+        c = c + 1;
+        t = setTimeout(timedCount, 1000);
+
+            if (c === 11) {
+            clearTimeout(t);
+
+                if (score >= 15) {
+                    function message() {
+                        console.log("Won");
+                    round++;
+                    setTimeout(roundTwo,1500);
+
+                    }
+
+                    setTimeout(message,1000);
+
+
+                } else {
+                    console.log("Failed")
+                }
+            }
+
+    }
+
+    timedCount();
+
+}
+
+
+function roundThree() {
+    displayRound.innerText = "Round: " + round;
+roundMessage.innerText = "Get 30 points to get to the next round";
+
+makeMoles(1000,4000);
+makeMoles(2000,3000);
+makeMoles(2500,3000);
+makeMoles(3000,2500);
+makeMoles(3500,3000);
+makeMoles(4000,1500);
+makeMoles(4500,2000);
+makeMoles(5000,2000);
+makeMoles(6000,1500);
+makeMoles(6500,2000);
+makeMoles(7000,1000);
+makeMoles(7500,1000);
+makeMoles(8000,2000);
+makeMoles(8500,1300);
+makeMoles(9000,1000);
+makeMoles(9500,2000);
+makeMoles(10000,1000);
+makeMoles(10500,2500);
+makeMoles(11000,1000);
+makeMoles(11500,1000);
+makeMoles(12000,1300);
+makeMoles(12500,1200);
+
+
+makeHappyMoles(2000,3000);
+makeHappyMoles(5500,3000);
+makeHappyMoles(6500,3000);
+makeHappyMoles(1500,2000);
+
+
+
+    // timedCount(21,30);
+
+
+
+
+
+
+
+}
+
+
+switch (round) {
+    case 1:
+    play = roundOne();
+    break;
+    case 2:
+    play = roundTwo();
+    break;
+    case 3:
+    play = roundThree();
+    break;
+    default:
+    play = console.log("No game");
+}
