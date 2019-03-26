fb9b01e1cd16212ebbb2bac8cb2cf2936dabb647
valenlyn <valenlynchua@gmail.com>
2019-03-26 14:14:22 +0800

    Made styling changes to modal

--- game/script.js
+++ game/script.js
@@ -37,13 +37,21 @@ var roundInfo = [
         "round": 3,
         "instructions": "Get 30 points!",
         "points": 30,
-        "timer": 15
+        "timer": 15,
+        "img": "art/sad-mole.png"
     },
     {
         "round": 4,
-        "instructions": "Squeeze pimples! Get 50 points.",
+        "instructions": " Rack up 50 points. Squeeze the pimples!",
         "points": 50,
-        "timer": 15
+        "timer": 15,
+        "img": "art/pimple.png"
+    },
+    {
+        "round": 5,
+        "instructions": "Whack!",
+        "points": 100000,
+        "timer": 1000000
     }
 ];
 
@@ -65,6 +73,8 @@ function showModal(roundNumber) {
 
     if (roundNumber === 2) {
         document.getElementById('example').setAttribute('class','happy-mole');
+    } if (roundNumber === 3) {
+        document.getElementById('example').classList.remove('happy-mole');
     }
 
 }
@@ -292,8 +302,8 @@ function restartGame() {
 function gameOver() {
 
 // hide game
-var game = document.getElementById('main');
-game.setAttribute('style','background-image:url("")');
+    var game = document.getElementById('main');
+    game.setAttribute('style','background-image:url("")');
 
 
         var gameOverPopUp = document.getElementsByClassName('game-over')[0];
@@ -307,6 +317,8 @@ game.setAttribute('style','background-image:url("")');
     if ( (score >= lowestTopScore) || (topScores.length <= 10)) {
         gameMsg.innerText = "New high score!";
         document.getElementsByClassName('input-button')[0].setAttribute('style','display:flex');
+    } else {
+         gameMsg.innerText = "Sorry you lost!";
     }
 
 
@@ -318,40 +330,40 @@ game.setAttribute('style','background-image:url("")');
 
 // 😭😭😭😭😭😭😭
 
-var c = 5;
-var t;
+// var c = 5;
+// var t;
 
-function countdownTimer(pointsNeeded) {
+// function countdownTimer(pointsNeeded) {
 
-        if (c <= 9) {
-            document.getElementById("timer").innerText = "00:0" + c;
-        } else if (c > 9) {
-             document.getElementById("timer").innerText = "00:" + c;
-        }
-
-
-        c = c - 1;
-
-        t = setTimeout(countdownTimer, 1000);
-
-            if (c === -1) {
-
-            clearTimeout(t);
-                if (score >= pointsNeeded) {
-                    function message() {
-                        console.log("Won");
-                    round++;
-                    }
-
-                    setTimeout(message,1000);
+//         if (c <= 9) {
+//             document.getElementById("timer").innerText = "00:0" + c;
+//         } else if (c > 9) {
+//              document.getElementById("timer").innerText = "00:" + c;
+//         }
 
 
-                } else {
-                    console.log("Failed")
-                }
-}
-}
-countdownTimer(2);
+//         c = c - 1;
+
+//         t = setTimeout(countdownTimer, 1000);
+
+//             if (c === -1) {
+
+//             clearTimeout(t);
+//                 if (score >= pointsNeeded) {
+//                     function message() {
+//                         console.log("Won");
+//                     round++;
+//                     }
+
+//                     setTimeout(message,1000);
+
+
+//                 } else {
+//                     console.log("Failed")
+//                 }
+// }
+// }
+// countdownTimer(2);
 
 
 function roundOne() {
@@ -431,7 +443,7 @@ function roundTwo() {
         }
     }
 
-function startRound() {
+    function startRound() {
     makeMoles(1000,4000);
     makeMoles(2000,3000);
     makeMoles(2500,3000);
@@ -457,72 +469,82 @@ function startRound() {
     makeHappyMoles(10000,3000);
     makeHappyMoles(12000,1800);
 
-        var c = 0;
-        var t;
+    var c = 0;
+    var t;
 
-        function timedCount() {
+    function timedCount() {
 
-            if (c <= 9) {
-                document.getElementById("timer").innerText = "00:0" + c;
-            } else if (c > 9) {
-                 document.getElementById("timer").innerText = "00:" + c;
-            }
-
-
-            c = c + 1;
-            t = setTimeout(timedCount, 1000);
-
-                if (c === 16) {
-                clearTimeout(t);
-
-                    if (score >= 15) {
-                            console.log("Won");
-                            round++;
-                            roundThree();
-
-                    } else {
-                        gameOver();
-                    }
-                }
+        if (c <= 9) {
+            document.getElementById("timer").innerText = "00:0" + c;
+        } else if (c > 9) {
+             document.getElementById("timer").innerText = "00:" + c;
         }
 
-        timedCount();
-}
+
+        c = c + 1;
+        t = setTimeout(timedCount, 1000);
+
+            if (c === 16) {
+            clearTimeout(t);
+
+                if (score >= 15) {
+                    round++;
+                    roundThree();
+
+                } else {
+                    status = "lost";
+                    gameOver();
+                }
+            }
+
+    }
+
+    timedCount();
+    }
 
 }
-
 
 function roundThree() {
 
-alert(roundInfo[3]["instructions"]);
+    showModal(3);
 
-makeMoles(1000,4000);
-makeMoles(2000,3000);
-makeMoles(2500,3000);
-makeMoles(3000,2500);
-makeMoles(3500,3000);
-makeMoles(4000,1500);
-makeMoles(4500,2000);
-makeMoles(5000,2000);
-makeMoles(6000,1500);
-makeMoles(6500,2000);
-makeMoles(7000,1000);
-makeMoles(7500,1000);
-makeMoles(8000,2000);
-makeMoles(8500,1300);
-makeMoles(9000,1000);
-makeMoles(9500,2000);
-makeMoles(10000,1000);
-makeMoles(10500,2500);
-makeMoles(11000,1000);
-makeMoles(11500,1000);
-makeMoles(12000,1300);
-makeMoles(12500,1200);
+    span.onclick = function() {
+        modal.style.display = "none";
+        startRound();
+    }
 
-makeHappyMoles(2000,3000);
-makeHappyMoles(5500,3000);
-makeHappyMoles(6500,3000);
-makeHappyMoles(1500,2000);
+    window.onclick = function(event) {
+        if (event.target == modal) {
+                modal.style.display = "none";
+                startRound();
+        }
+    }
+
+    function startRound() {
+    makeMoles(1000,4000);
+    makeMoles(2000,3000);
+    makeMoles(2500,3000);
+    makeMoles(3000,2500);
+    makeMoles(3500,3000);
+    makeMoles(4000,1500);
+    makeMoles(4500,2000);
+    makeMoles(5000,3000);
+    makeMoles(6000,1500);
+    makeMoles(6500,2000);
+    makeMoles(7000,3000);
+    makeMoles(7500,2000);
+    makeMoles(8000,3000);
+    makeMoles(8500,2300);
+    makeMoles(9000,2000);
+    makeMoles(9500,3000);
+    makeMoles(10000,4000);
+    makeMoles(11000,3000);
+
+    makeHappyMoles(2000,3000);
+    makeHappyMoles(5500,3000);
+    makeHappyMoles(6500,3000);
+    makeHappyMoles(10000,3000);
+    makeHappyMoles(12000,1800);
 
     var c = 0;
     var t;
@@ -542,18 +564,12 @@ makeHappyMoles(1500,2000);
             if (c === 16) {
             clearTimeout(t);
 
-                if (score >= 30) {
-                    function message() {
-                        console.log("Won");
+                if (score >= 15) {
                     round++;
-                    setTimeout(roundFour,2000);
-                    }
-
-                    setTimeout(message,1000);
-
+                    roundFour();
 
                 } else {
-                    console.log("Failed")
+                    status = "lost";
                     gameOver();
                 }
             }
@@ -561,86 +577,127 @@ makeHappyMoles(1500,2000);
     }
 
     timedCount();
+    }
 
 }
+
+
+
+
 
 function roundFour() {
 
-alert(roundInfo[4]["instructions"]);
+showModal(4);
 
-makeMoles(1000,2000);
-makeMoles(2000,2500);
-makeMoles(2500,2700);
-makeMoles(3000,1500);
-makeMoles(3500,2000);
-makeMoles(4000,2500);
-makeMoles(4500,1000);
-makeMoles(5000,3000);
-makeMoles(6300,1500);
-makeMoles(7000,2500);
-makeMoles(8000,1500);
-makeMoles(9000,2500);
-makeMoles(10500,2500);
-makeMoles(11000,1000);
-makeMoles(11500,1000);
-makeMoles(12000,1300);
-makeMoles(12500,2200);
-makeMoles(13000,2000);
-
-makePimples(2000,2000);
-makePimples(5000,1500);
-makePimples(8000,2000);
-makePimples(10000,3000);
-makePimples(12000,1500);
-
-makeHappyMoles(1500,2000);
-makeHappyMoles(3000,3000);
-makeHappyMoles(5500,3000);
-makeHappyMoles(6500,3000);
-makeHappyMoles(11700,2500);
-makeHappyMoles(12500,1300);
-
-var c = 0;
-    var t;
-
-    function timedCount() {
-
-        if (c <= 9) {
-            document.getElementById("timer").innerText = "00:0" + c;
-        } else if (c > 9) {
-             document.getElementById("timer").innerText = "00:" + c;
-        }
-
-
-        c = c + 1;
-        t = setTimeout(timedCount, 1000);
-
-            if (c === 16) {
-            clearTimeout(t);
-
-                if (score >= 50) {
-                    function message() {
-                        console.log("Won");
-                    round++;
-                    setTimeout(roundFour,2000);
-                    }
-
-                    setTimeout(message,1000);
-
-
-                } else {
-                    gameOver();
-                }
-            }
-
+    span.onclick = function() {
+        modal.style.display = "none";
+        startRound();
     }
 
-    timedCount();
+    window.onclick = function(event) {
+        if (event.target == modal) {
+                modal.style.display = "none";
+                startRound();
+        }
+    }
+
+function startRound() {
+    makeMoles(1000,2000);
+    makeMoles(2000,2500);
+    makeMoles(2500,2700);
+    makeMoles(3000,1500);
+    makeMoles(3500,2000);
+    makeMoles(4000,2500);
+    makeMoles(4500,1000);
+    makeMoles(5000,3000);
+    makeMoles(6300,1500);
+    makeMoles(7000,2500);
+    makeMoles(8000,1500);
+    makeMoles(9000,2500);
+    makeMoles(10500,2500);
+    makeMoles(11000,1000);
+    makeMoles(11500,1000);
+    makeMoles(12000,1300);
+    makeMoles(12500,2200);
+    makeMoles(13000,2000);
+
+    makePimples(2000,2000);
+    makePimples(5000,1500);
+    makePimples(8000,2000);
+    makePimples(10000,3000);
+    makePimples(12000,1500);
+
+    makeHappyMoles(1500,2000);
+    makeHappyMoles(3000,3000);
+    makeHappyMoles(5500,3000);
+    makeHappyMoles(6500,3000);
+    makeHappyMoles(11700,2500);
+    makeHappyMoles(12500,1300);
+
+    var c = 0;
+        var t;
+
+        function timedCount() {
+
+            if (c <= 9) {
+                document.getElementById("timer").innerText = "00:0" + c;
+            } else if (c > 9) {
+                 document.getElementById("timer").innerText = "00:" + c;
+            }
+
+
+            c = c + 1;
+            t = setTimeout(timedCount, 1000);
+
+                if (c === 16) {
+                clearTimeout(t);
+
+                    if (score >= 50) {
+                        round++;
+                        roundFive();
+                        }
+
+
+                    } else {
+                        gameOver();
+                    }
+                }
+
+        }
+
+        timedCount();
+}
+
+
+
+function roundFive() {
+
+showModal(5);
+
+    span.onclick = function() {
+        modal.style.display = "none";
+        startRound();
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+                modal.style.display = "none";
+                startRound();
+        }
+    }
+
+
+function startRound() {
+
+// random number between 1000 and 4000
+    var randomInterval = Math.floor(Math.random() * ((4000-1000)+1) + 4000);
+
+    for (var i = 0; i < 1000; i++) {
+        makeMoles(i * 1000,randomInterval);
+    }
 
 }
 
-function roundFive() {
-    // make happy moles to fill the screen, use mouseover event listener to trigger point allocation
 }
 
 
